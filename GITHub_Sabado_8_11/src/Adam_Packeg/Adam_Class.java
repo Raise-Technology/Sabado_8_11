7125f606a4a0e03bd755a95f1fa236e4c5d8ff46
AGEBW <a.d.river@hotmail.com>
2017-11-18 11:28:46 -0700

    agregue la clase Muajajajaj y ya se muestra la ventana

--- GITHub_Sabado_8_11/src/Adam_Packeg/Adam_Class.java
+++ GITHub_Sabado_8_11/src/Adam_Packeg/Adam_Class.java
@@ -1,16 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Adam_Packeg;
 
-/**
- *
- * @author Cesar Programacion
- */
+
 public class Adam_Class {
-    Muajajaj m= new Muajajaj();
+
+    public Adam_Class(){
+        
+        Muajajaj m= new Muajajaj();
     
-    m.setVisible(true);
+      m.setVisible(true); 
+    }
+  
+  
 }
