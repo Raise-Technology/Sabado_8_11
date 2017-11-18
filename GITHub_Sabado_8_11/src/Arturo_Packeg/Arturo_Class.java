98d36594c815804e4bdd57a154ee044ab6f2feb5
Cesar Programacion <Cesar Programacion@192.168.1.78>
2017-11-18 11:47:59 -0600

    Se agrego tres paquetes nuevos con sus respectivas clases para que los desarrolladores puedan trabajarlos.
    Gilberto

---
+++ GITHub_Sabado_8_11/src/Arturo_Packeg/Arturo_Class.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arturo_Packeg;
+
+/**
+ *
+ * @author Cesar Programacion
+ */
+public class Arturo_Class {
+    
+}
