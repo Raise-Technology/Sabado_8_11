88b9fce37796071f32dee53a6776d88b87d203c3
Cesar Programacion <Cesar Programacion@192.168.1.78>
2017-11-18 11:03:05 -0600

    Otra vez
    Gilberto

---
+++ GITHub_Sabado_8_11/src/General/Inicio.java
@@ -0,0 +1,16 @@
+package General;
+
+/**
+ *
+ * @author Cesar Programacion
+ */
+public class Inicio {
+
+    public static void main(String[] args) {
+        
+        Principal P = new Principal();
+        P.setVisible(true);
+        
+    }
+    
+}
